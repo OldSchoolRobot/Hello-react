a23276a9e916d4c1c5e19d806282112b1f6a912e
Roberto V <bobsteadyrock@gmail.com>
2019-02-15 22:07:22 -0600

    First commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <div>
+        Header
+      </div>
+      <div>
+        <div>
+          List
+          </div>
+        <div>
+          Adds
+          </div>
+      </div>
+      </div>
+    );
+  }
+}
+
+export default App;
