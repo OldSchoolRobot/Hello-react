f1af8fdb587f260d15d4252d2d3cea553863458f
Roberto V <bobsteadyrock@gmail.com>
2019-02-17 20:42:20 -0600

    Create Detail and Add components

--- src/components/Add.js
+++ src/components/Add.js
@@ -4,7 +4,21 @@ class Add extends Component {
   render() {
     return (
         <div className="add">
-          Add
+          <h1>Estás agregando un nuevo elemento</h1>
+          <div className="content column">
+            <label className="column" for="name">
+              Nombre:
+              <input type="text" id="name" name="name" />
+            </label>
+            <label className="column" for="description">
+              Descripción:
+              <textarea id="description" name="description" rows="8" ></textarea>
+            </label>
+            <div className="row">          
+              <button className="button mr">Cancelar</button>
+              <button className="button">Crear</button>
+            </div>
+          </div>
         </div>
     );
   }
--- src/components/Detail.js
+++ src/components/Detail.js
@@ -4,7 +4,21 @@ class Detail extends Component {
   render() {
     return (
         <div className="detail">
-          Detail
+          <h1>Estás editando un nuevo elemento</h1>
+          <div className="content column">
+            <label className="column" for="name">
+              Nombre:
+              <input type="text" id="name" name="name" />
+            </label>
+            <label className="column" for="description">
+              Descripción:
+              <textarea id="description" name="description" rows="8" ></textarea>
+            </label>
+            <div className="row">          
+              <button className="button mr">Cancelar</button>
+              <button className="button">Guardar</button>
+            </div>
+          </div>
         </div>
     );
   }
